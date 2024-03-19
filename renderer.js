8dfa79afb5906dd41c20cf28989965669a2ece43
guest271314 <guest271314@gmail.com>
2024-03-18 20:15:10 -0700

    Encode to WAV
    Pass buffer to AudioContext.decodeAudioData(), pass AudioBuffer to WavAudioEncoder.write(), set <audio> element src to WAV file

--- renderer.js
+++ renderer.js
@@ -1,3 +1,4 @@
+import { WavAudioEncoder } from './WavAudioEncoder.js';
 // Get the necessary DOM elements
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
@@ -10,26 +11,36 @@ const startRecording = async () => {
   const chunks = [];
   const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
   mediaRecorder = new MediaRecorder(stream);
-  mediaRecorder.ondataavailable = event => {
-      chunks.push(event.data);
+  mediaRecorder.ondataavailable = (event) => {
+    chunks.push(event.data);
   };
   mediaRecorder.onstop = () => {
-      const blob = new Blob(chunks);
+    const blob = new Blob(chunks);
+    const ac = new AudioContext();
+    // Convert Blob to buffer
+    blob
+      .arrayBuffer()
+      .then(async (buffer) => {
+        const audioBuffer = await ac.decodeAudioData(buffer);
+        const wav = new WavAudioEncoder({
+          numberOfChannels: audioBuffer.numberOfChannels,
+          sampleRate: ac.sampleRate,
+        });
+        wav.write(audioBuffer);
+        const data = await wav.encode();
+        await ac.close();
 
-      // Convert Blob to buffer
-      blob.arrayBuffer().then(buffer => {
-          window.sendAudioToMain.send(buffer);
-      }).catch(error => {
-          console.error('Error converting Blob to buffer:', error);
+        const audioUrl = URL.createObjectURL(data);
+        const audioElement = document.getElementById('audioElement');
+        audioElement.src = audioUrl;
+        // window.sendAudioToMain.send(await data.arrayBuffer());
+      })
+      .catch((error) => {
+        console.error('Error converting Blob to buffer:', error);
       });
-
-      const audioUrl = URL.createObjectURL(blob);
-      const audioElement = document.getElementById('audioElement');
-      audioElement.src = audioUrl;
   };
   mediaRecorder.start();
 };
-
 
 // Function to stop recording
 const stopRecording = () => {
